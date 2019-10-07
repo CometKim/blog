667440165da71e9f1d37e86ac69cb2ac2353bb48
iamchanii <iamchanii.0310@gmail.com>
2019-10-07 17:17:13 +0900

    Update favicon image

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
                 background_color: `#222`,
                 theme_color: `#222`,
                 display: `minimal-ui`,
-                icon: `src/images/profile.jpeg`, // This path is relative to the root of the site.
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
             },
         },
         {

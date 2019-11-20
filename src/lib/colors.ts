eb572648acfa7ffc3a2ee7d1c03294614c0f85dc
iamchanii <iamchanii.0310@gmail.com>
2019-11-20 22:19:32 +0900

    Update style

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -3,7 +3,7 @@ import oc from 'open-color';
 export default {
     white: oc.white,
     primary: oc.cyan[8],
-    content: oc.gray[8],
+    content: oc.gray[7],
     layoutBackground: oc.gray[0],
     headerBackground: oc.gray[0],
     border: oc.gray[2],

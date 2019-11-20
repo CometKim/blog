7da834fcb501564b7292bbd614dfb40f11a0986b
iamchanii <iamchanii.0310@gmail.com>
2019-11-20 18:16:08 +0900

    Fix build error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,5 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import './src/styles/prism-nord.css';
 import './src/styles/styles.scss';
-import { BP_LG, BP_MD, BP_SM, BP_XL, BP_XS } from './src/lib/breakpoint';
+import wrapRootElement from './src/lib/wrapRootElement';
 
-const theme = {
-    breakpoints: {
-        xs: BP_XS,
-        sm: BP_SM,
-        md: BP_MD,
-        lg: BP_LG,
-        xl: BP_XL,
-    },
-};
-
-export const wrapRootElement = ({ element }) => {
-    return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
-};
+export { wrapRootElement };

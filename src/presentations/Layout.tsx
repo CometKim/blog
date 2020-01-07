d14b6164045effe01ca1c78924eeecd5e3d69848
iamchanii <iamchanii.0310@gmail.com>
2020-01-07 20:35:00 +0900

    Implement header reveal and hide by scroll event

--- src/presentations/Layout.tsx
+++ src/presentations/Layout.tsx
@@ -1,21 +1,88 @@
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import tw from 'tailwind.macro';
 
 const headerCss = css`
   background-color: rgba(255, 255, 255, 0.75);
 
-  @supports (backdrop-filter: blur(4px)) {
-    backdrop-filter: blur(4px);
+  @supports (backdrop-filter: blur(5px)) {
+    backdrop-filter: blur(5px);
     ${tw`border-b border-gray-300`}
+  }
+
+  transition: transform 250ms ease;
+
+  &.header-scroll-up {
+    transform: none;
+  }
+
+  &.header-scroll-down {
+    transform: translate3d(0, -100%, 0);
   }
 `;
 
+const removeClass = (element: HTMLElement, className: string) => {
+  element.classList.remove(className);
+};
+
+const addClass = (element: HTMLElement, className: string) => {
+  element.classList.add(className);
+};
+
+const isContainClass = (element: HTMLElement, className: string) => {
+  return element.classList.contains(className);
+};
+
+const headerScrollUp = 'header-scroll-up';
+const headerScrollDown = 'header-scroll-down';
+const headerHideOffset = 128;
+
 const Layout: React.FC = React.memo(({ children }) => {
+  const headerRef = useRef<HTMLElement>();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    let lastYOffset = 0;
+
+    const onWindowScroll = () => {
+      if (!headerRef.current) {
+        return;
+      }
+
+      const currentYOffset = window.pageYOffset;
+      const el = headerRef.current;
+
+      if (currentYOffset === 0) {
+        removeClass(el, headerScrollUp);
+        return;
+      }
+
+      if (currentYOffset > lastYOffset && currentYOffset >= headerHideOffset && !isContainClass(el, headerScrollDown)) {
+        addClass(el, headerScrollDown);
+        removeClass(el, headerScrollUp);
+      } else if (currentYOffset < lastYOffset && isContainClass(el, headerScrollDown)) {
+        addClass(el, headerScrollUp);
+        removeClass(el, headerScrollDown);
+      }
+
+      lastYOffset = currentYOffset;
+    };
+
+    window.addEventListener('scroll', onWindowScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onWindowScroll);
+    };
+  }, []);
+
   return (
     <>
       <header
+        ref={headerRef}
         css={headerCss}
         className="flex justify-between items-center fixed top-0 left-0 right-0 z-10 md:px-8 md:py-4 md:h-16 px-8 h-12"
       >

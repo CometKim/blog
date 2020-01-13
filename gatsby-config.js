7c065ecc5b59890bbb6d7034cb0ec0386b79bb4f
iamchanii <iamchanii.0310@gmail.com>
2020-01-13 20:33:31 +0900

    Fix rss feed
    Signed-off-by: iamchanii <iamchanii.0310@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,7 @@ module.exports = {
                     frontmatter {
                       title
                       date
+                      slug
                     }
                   }
                 }

4b2b892cc4eae772f5687082ee82ba8fd78eb35a
iamchanii <iamchanii.0310@gmail.com>
2020-02-10 21:15:22 +0900

    Change dateformat

--- src/lib/formatDatetime.ts
+++ src/lib/formatDatetime.ts
@@ -1,19 +1,5 @@
-import { formatDistanceToNow } from 'date-fns';
-import { parseISO } from 'date-fns/fp';
-import ko from 'date-fns/locale/ko';
-import * as R from 'remeda';
-
 const formatDatetime = (date: string): string => {
-  return R.pipe(
-    date,
-    parseISO,
-    date => {
-      return formatDistanceToNow(date, {
-        locale: ko,
-        addSuffix: true,
-      });
-    },
-  );
+  return new Intl.DateTimeFormat('ko').format(new Date(date));
 };
 
 export default formatDatetime;
